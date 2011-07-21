dfb27e8f4609a9dede48c0ed4a5925f9b9acc4d4
Aishwarya Singhal <asinghal79@gmail.com>
2011-07-21 15:48:20 +0530

    added ability to play

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,46 +2,70 @@ class PicturesController < ApplicationController
   # GET /pictures
   # GET /pictures.xml
   def index
-    @pictures = Picture.all
-
+    @pictures = Picture.pickRandom
+    
+    picture = @pictures[rand(@pictures.size)]
+    
+    @correct_id = picture.id
+    @picture_name = picture.name
+    
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @pictures }
     end
   end
-
+  
+  # GET /pictures/1
+  # GET /pictures/1.xml
+  def play
+    @picture = Picture.find(params[:id])
+    
+    if params[:correctid] == params[:id]
+      respond_to do |format|
+        format.html { render :action => "show" }
+        format.xml  { render :xml => @picture }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to(pictures_url) }
+        format.xml  { head :ok }
+      end
+    end
+    
+  end
+  
   # GET /pictures/1
   # GET /pictures/1.xml
   def show
     @picture = Picture.find(params[:id])
-
+    
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @picture }
     end
   end
-
+  
   # GET /pictures/new
   # GET /pictures/new.xml
   def new
     @picture = Picture.new
-
+    
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @picture }
     end
   end
-
+  
   # GET /pictures/1/edit
   def edit
     @picture = Picture.find(params[:id])
   end
-
+  
   # POST /pictures
   # POST /pictures.xml
   def create
     @picture = Picture.new(params[:picture])
-
+    
     respond_to do |format|
       if @picture.save
         format.html { redirect_to(@picture, :notice => 'Picture was successfully created.') }
@@ -52,12 +76,12 @@ class PicturesController < ApplicationController
       end
     end
   end
-
+  
   # PUT /pictures/1
   # PUT /pictures/1.xml
   def update
     @picture = Picture.find(params[:id])
-
+    
     respond_to do |format|
       if @picture.update_attributes(params[:picture])
         format.html { redirect_to(@picture, :notice => 'Picture was successfully updated.') }
@@ -68,13 +92,13 @@ class PicturesController < ApplicationController
       end
     end
   end
-
+  
   # DELETE /pictures/1
   # DELETE /pictures/1.xml
   def destroy
     @picture = Picture.find(params[:id])
     @picture.destroy
-
+    
     respond_to do |format|
       format.html { redirect_to(pictures_url) }
       format.xml  { head :ok }
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,7 @@
 class Picture < ActiveRecord::Base
+  
+  def self.pickRandom
+#    SELECT id FROM pictures ORDER BY RANDOM() limit 3
+    find(:all,  :order => 'RANDOM() DESC', :limit => 3)
+  end
 end
